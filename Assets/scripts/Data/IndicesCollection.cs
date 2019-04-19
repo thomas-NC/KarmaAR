18b650cce66620c450d4af28e4a5140c15df3f57
ThomasNC <thomas.nguyencong@ynov.com>
2019-04-19 08:59:08 +0200

    xml refait

--- Assets/scripts/Data/IndicesCollection.cs
+++ Assets/scripts/Data/IndicesCollection.cs
@@ -2,6 +2,7 @@
 using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
+using UnityEngine;
 
 [XmlRoot("IndicesCollection")]
 public class IndicesCollection
@@ -12,12 +13,25 @@ public class IndicesCollection
 
     public static IndicesCollection loadData(string path)
     {
-        XmlSerializer serializer = new XmlSerializer(typeof(IndicesCollection));
+        /*XmlSerializer serializer = new XmlSerializer(typeof(IndicesCollection));
         using (FileStream stream = new FileStream(path, FileMode.Open))
         {
             IndicesCollection indices = serializer.Deserialize(stream) as IndicesCollection;
             return indices;
-        }
+        }*/
+       
+            TextAsset monxml = Resources.Load<TextAsset>(path);
+          
+            XmlSerializer serializer = new XmlSerializer(typeof(IndicesCollection));
+
+            StringReader reader = new StringReader(monxml.text);
+
+            IndicesCollection indices = serializer.Deserialize(reader) as IndicesCollection;
+
+            return indices;
         
+        
+
+
     }
 }
--- Assets/scripts/Data/IndicesReader.cs
+++ Assets/scripts/Data/IndicesReader.cs
@@ -9,32 +9,48 @@ public class IndicesReader : MonoBehaviour
     public Text indice;
     public Text index;
     public GameObject ficheIndice;
-    private IndicesCollection data = new IndicesCollection();
+    private IndicesCollection ic = new IndicesCollection();
+
+    public const string path = "Indices";
 
     // Start is called before the first frame update
     void Start()
     {
-        data = IndicesCollection.loadData("Assets/Data/Indices.xml");
+
+        IndicesCollection ic = IndicesCollection.loadData(path);
+
+        foreach (Indice item in ic.indicesCollection)
+        {
+            //Debug.Log("--// id: " + item.id + "\n contenu = " + item.content + "//--");
+            if(item.id == "99")
+            {
+                Debug.Log(item.content);
+            }
+        }
+        /*data = IndicesCollection.loadData(Application.persistentDataPath + "/Indices.xml");
+        Debug.Log(Application.persistentDataPath);
 
         foreach (var item in data.indicesCollection)
         {
             Debug.Log("--id: " + item.id + "\n contenu = " + item.content + "--");
             
-        }
+        } */
     }
 
     public void ShowIndice()
     {
+        IndicesCollection ic = IndicesCollection.loadData(path);
         indice.text = "";
-        
-        foreach (var item in data.indicesCollection)
+
+        foreach (var item in ic.indicesCollection)
         {
             if(index.text == item.id)
             {
                 indice.text = item.content;
+                ficheIndice.SetActive(true);
             }
         }
-        ficheIndice.SetActive(true);
+        
         index.text = "";
     }
 
